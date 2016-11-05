fe3619892fd35504df3d47012f26c280b1ed1baa
Yueyi <annieyueyiwang@berkeley.edu>
2016-11-05 15:59:56 -0700

    first line

--- projects/proj3_measurement/traceroute.py
+++ projects/proj3_measurement/traceroute.py
@@ -1,10 +1,14 @@
 import subprocess
+import sys
 
 def run_traceroute(hostnames, num_packets, output_filename):
 	for host in hostnames:
-		output = subprocess.check_output("traceroute -a -q " + str(num_packets) + " " + host, shell=True)
+		output = subprocess.check_output("traceroute -a -q " + str(num_packets) + " " + host + " 2>&1", shell=True)
 		with open(output_filename, "a") as f:
 			f.write(output)
+
+def parse_traceroute(raw_traceroute_filename, output_filename):
+	
 
 run_traceroute(["google.com"], 2, "raw_trace.out")
 
