f291e052c1450c1ae38ea5ad9ea8877dfe601eb7
Yueyi <annieyueyiwang@berkeley.edu>
2016-11-06 02:30:59 -0800

    traceroute parsing

--- projects/proj3_measurement/traceroute.py
+++ projects/proj3_measurement/traceroute.py
@@ -1,5 +1,7 @@
 import subprocess
 import sys
+import re
+import json
 
 def run_traceroute(hostnames, num_packets, output_filename):
 	for host in hostnames:
@@ -13,25 +15,38 @@ def parse_traceroute(raw_traceroute_filename, output_filename):
 	host_to_trace = {}
 	firstLine = f.readline()
 	firstHost = firstLine.split(" ")[2]
-	host_to_trace.add[firstHost]
 
 	currHost = firstHost
-	currHop = 1
-	line = f.readLine()
+	currHop = 0
+	line = f.readline()
 	while (line != ""):
 		if line.split(" ")[0] == "traceroute":
 			currHost = line.split(" ")[2]
 			currHop = 0
 		else:
-			elements = f.split(" ")
-			if elements[0].isdigit():
-				currHop = elements[0]
+			data = line.split(" ")
+			# example data:
+			# ['', '6', '', '[AS15169]', '108.170.242.241', '(108.170.242.241)', '', '3.641', 'ms\n']
+			if data[1].isdigit():
+				currHop = int(data[1])
+			ASN = data[3].replace('[', '').replace(']', '').replace('A', '').replace('S', '')
+			name = data[4]
+			ip = data[5].replace('(', '').replace(')', '')
 			if currHost not in host_to_trace:
-				host_to_trace[currHost] = [[{info here}]]
+				host_to_trace[currHost] = [[{'ip': ip, 'name': name, 'ASN': ASN}]]
+			else:
+				if len(host_to_trace[currHost]) == currHop:
+					host_to_trace[currHost][currHop-1] += [{'ip': ip, 'name': name, 'ASN': ASN}]
+				else:
+					host_to_trace[currHost] += [[{'ip': ip, 'name': name, 'ASN': ASN}]]
 
-		line = f.readLine()
+		line = f.readline()
+
+	with open(output_filename, 'w') as fp:
+		json.dump(host_to_trace, fp)
+	# print(host_to_trace)
 
 
-
-run_traceroute(["google.com"], 2, "raw_trace.out")
+parse_traceroute('raw_trace.out', 'traceroute.json')
+#run_traceroute(["google.com"], 2, "raw_trace.out")
 
