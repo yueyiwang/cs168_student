0f1cd55d1a5a3052147728a9efc4d02575080e7f
Joanna Yang <joannayangg@berkeley.edu>
2016-11-07 20:07:11 -0800

    better parsing

--- projects/proj3_measurement/traceroute.py
+++ projects/proj3_measurement/traceroute.py
@@ -2,23 +2,35 @@ import subprocess
 import sys
 import re
 import json
+import time
 
 def run_traceroute(hostnames, num_packets, output_filename):
+	print("running trace route")
+	with open(output_filename, "w") as f:
+		t = str(time.time())
+		print(t)
+		f.write(t + '\n')
 	for host in hostnames:
+		print(host)
 		output = subprocess.check_output("traceroute -a -q " + str(num_packets) + " " + host + " 2>&1", shell=True)
 		with open(output_filename, "a") as f:
 			f.write(output)
 
 def parse_traceroute(raw_traceroute_filename, output_filename):
+	print("parsing trace route")
 	f = open(raw_traceroute_filename, 'r')
 	#readline returns blank string if at the end of string
 	host_to_trace = {}
+	time = f.readline().split(" ")[0]
+	print(time)
+	host_to_trace["timestamp"] = time
 	firstLine = f.readline()
+	print(firstLine)
 	firstHost = firstLine.split(" ")[2]
-
 	currHost = firstHost
 	currHop = 0
 	line = f.readline()
+	print(currHost)
 	while (line != ""):
 		if line.split(" ")[0] == "*":
 			continue
@@ -44,11 +56,13 @@ def parse_traceroute(raw_traceroute_filename, output_filename):
 
 		line = f.readline()
 
-	with open(output_filename, 'w') as fp:
+	with open(output_filename, 'a') as fp:
 		json.dump(host_to_trace, fp)
+		fp.write('\n')
 	# print(host_to_trace)
 
 
-parse_traceroute('raw_trace.out', 'traceroute.json')
-#run_traceroute(["google.com"], 2, "raw_trace.out")
+#parse_traceroute('raw_trace.out', 'traceroute.json')
+#run_traceroute(["google.com", "facebook.com", "www.berkeley.edu", "allspice.lcs.mit.edu", "todayhumor.co.kr", "www.city.kobe.lg.jp","www.vutbr.cz", "zanvarsity.ac.tz"], 5, "raw_tr_a.out")
+parse_traceroute('raw_tr_a.out', 'trace_a.json')
 
